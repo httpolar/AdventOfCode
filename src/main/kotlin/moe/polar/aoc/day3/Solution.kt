34548439d03a2b34c71a0fd1608a2fc2ab50b6ee
httpolar <http@polar.moe>
2023-12-05 00:36:03 +0400

    Refactored Day 3

--- src/main/kotlin/moe/polar/aoc/day3/Solution.kt
+++ src/main/kotlin/moe/polar/aoc/day3/Solution.kt
@@ -7,58 +7,137 @@ class Solution : DaySolution() {
     private val lines = text.lines()
     private val arr = Array(lines.size) { row -> CharArray(lines.first().length) { col -> lines[row][col] } }
 
-    private fun Array<CharArray>.hasValueAt(rowIdx: Int, colIdx: Int): Boolean {
-        return rowIdx >= 0 && rowIdx <= this.lastIndex && colIdx >= 0 && colIdx <= this.first().lastIndex
-    }
 
-    private data class MatrixPos(var rowIdx: Int, var colIdx: Int)
+    private inner class MatrixPos(var rowIdx: Int, var colIdx: Int) {
+        val value: Char? get() = arr.getOrNull(rowIdx)?.getOrNull(colIdx)
 
-    private fun MatrixPos.exists(): Boolean {
-        return arr.hasValueAt(rowIdx, colIdx)
-    }
+        fun getDigitSequenceOrNull(): DigitSequence? {
+            var left = colIdx
+            var foundLeft = false
 
-    private val MatrixPos.value: Char?
-        get() = arr.getOrNull(rowIdx)?.getOrNull(colIdx)
+            if (!arr[rowIdx][left].isDigit())
+                return null
 
-    private data class DigitSequence(var rowIdx: Int, var startIdx: Int, var endIdx: Int)
+            var right = colIdx + 1
+            var foundRight = false
 
-    private val DigitSequence.indices: IntRange
-        get() = startIdx..<endIdx + 1
+            while (!foundLeft || !foundRight) {
+                val chLeft = arr[rowIdx].getOrNull(left)
+                val chRight = arr[rowIdx].getOrNull(right)
 
-    private fun DigitSequence.toInt(): Int {
-        var num = arr[rowIdx][startIdx].digitToInt()
-        if (startIdx == endIdx)
-            return num
+                val chNextLeft = arr[rowIdx].getOrNull(left - 1)
+                val chNextRight = arr[rowIdx].getOrNull(right + 1)
 
-        for (colIdx in startIdx + 1..endIdx) {
-            num *= 10
-            num += arr[rowIdx][colIdx].digitToInt()
-        }
+                // make sure current characters are digits
+                if (chLeft?.isDigit() != true) {
+                    foundLeft = true
+                    left++
+                }
 
-        return num
-    }
+                if (chRight?.isDigit() != true) {
+                    foundRight = true
+                    right--
+                }
 
-    private fun DigitSequence.hasNeighborSymbol(): Boolean {
-        val neighbors = ArrayList<MatrixPos>()
-        neighbors.add(MatrixPos(rowIdx, startIdx - 1))
-        neighbors.add(MatrixPos(rowIdx, endIdx + 1))
+                // check if next characters are digits
+                if (!foundLeft && chNextLeft?.isDigit() != true) {
+                    foundLeft = true
+                }
 
-        for (j in startIdx - 1..endIdx + 1) {
-            neighbors.add(MatrixPos(rowIdx - 1, j))
-            neighbors.add(MatrixPos(rowIdx + 1, j))
-        }
+                if (!foundRight && chNextRight?.isDigit() != true) {
+                    foundRight = true
+                }
 
-        neighbors.forEach { pos ->
-            if (pos.exists()) {
-                val cell = arr[pos.rowIdx][pos.colIdx]
+                // move pointers
+                if (!foundLeft) {
+                    left--
+                }
 
-                if (!cell.isDigit() && cell != '.') {
-                    return true
+                if (!foundRight) {
+                    right++
                 }
             }
+
+            return DigitSequence(rowIdx, left, right)
         }
 
-        return false
+        fun gearValue(): Int? {
+            if (value == null)
+                return null
+
+            val ch = arr[rowIdx][colIdx]
+            if (ch != '*')
+                return null
+
+            // TODO: could optimize this thing here so we avoid usage of Set later amd for that we need to find overlapping sequences
+            val neighbors = ArrayList<MatrixPos>()
+
+            for (i in rowIdx - 1..rowIdx + 1) {
+                for (j in colIdx - 1..colIdx + 1) {
+                    if (i == rowIdx && j == colIdx)
+                        continue
+
+                    val pos = MatrixPos(i, j)
+                    if (pos.value != null) {
+                        neighbors.add(pos)
+                    }
+                }
+            }
+
+            val sequences = mutableSetOf<DigitSequence>()
+
+            neighbors.forEach { pos ->
+                if (pos.value?.isDigit() == true) {
+                    val digitSeq = pos.getDigitSequenceOrNull()
+                    if (digitSeq != null) {
+                        sequences.add(digitSeq)
+                    }
+                }
+            }
+
+            if (sequences.size != 2)
+                return null
+
+            return sequences.map { it.toInt() }.reduce { acc: Int, current: Int -> acc * current }
+        }
+    }
+
+    private inner class DigitSequence(var rowIdx: Int, var startIdx: Int, var endIdx: Int) {
+        fun toInt(): Int {
+            var num = arr[rowIdx][startIdx].digitToInt()
+            if (startIdx == endIdx)
+                return num
+
+            for (colIdx in startIdx + 1..endIdx) {
+                num *= 10
+                num += arr[rowIdx][colIdx].digitToInt()
+            }
+
+            return num
+        }
+
+        fun hasNeighborSymbol(): Boolean {
+            val neighbors = ArrayList<MatrixPos>()
+            neighbors.add(MatrixPos(rowIdx, startIdx - 1))
+            neighbors.add(MatrixPos(rowIdx, endIdx + 1))
+
+            for (j in startIdx - 1..endIdx + 1) {
+                neighbors.add(MatrixPos(rowIdx - 1, j))
+                neighbors.add(MatrixPos(rowIdx + 1, j))
+            }
+
+            neighbors.forEach { pos ->
+                if (pos.value != null) {
+                    val cell = arr[pos.rowIdx][pos.colIdx]
+
+                    if (!cell.isDigit() && cell != '.') {
+                        return true
+                    }
+                }
+            }
+
+            return false
+        }
     }
 
     override fun part1() {
@@ -111,96 +190,6 @@ class Solution : DaySolution() {
         }
 
         println(sum)
-    }
-
-
-    private fun MatrixPos.getDigitSequence(): DigitSequence? {
-        var left = colIdx
-        var foundLeft = false
-
-        if (!arr[rowIdx][left].isDigit())
-            return null
-
-        var right = colIdx + 1
-        var foundRight = false
-
-        while (!foundLeft || !foundRight) {
-            val chLeft = arr[rowIdx].getOrNull(left)
-            val chRight = arr[rowIdx].getOrNull(right)
-
-            val chNextLeft = arr[rowIdx].getOrNull(left - 1)
-            val chNextRight = arr[rowIdx].getOrNull(right + 1)
-
-            // make sure current characters are digits
-            if (chLeft?.isDigit() != true) {
-                foundLeft = true
-                left++
-            }
-
-            if (chRight?.isDigit() != true) {
-                foundRight = true
-                right--
-            }
-
-            // check if next characters are digits
-            if (!foundLeft && chNextLeft?.isDigit() != true) {
-                foundLeft = true
-            }
-
-            if (!foundRight && chNextRight?.isDigit() != true) {
-                foundRight = true
-            }
-
-            // move pointers
-            if (!foundLeft) {
-                left--
-            }
-
-            if (!foundRight) {
-                right++
-            }
-        }
-
-        return DigitSequence(rowIdx, left, right)
-    }
-
-    private fun MatrixPos.gearValue(): Int? {
-        if (!exists())
-            return null
-
-        val ch = arr[rowIdx][colIdx]
-        if (ch != '*')
-            return null
-
-        val neighbors = ArrayDeque<MatrixPos>()
-
-        for (i in rowIdx - 1..rowIdx + 1) {
-            for (j in colIdx - 1..colIdx + 1) {
-                if (i == rowIdx && j == colIdx)
-                    continue
-
-                val pos = MatrixPos(i, j)
-                if (pos.exists()) {
-                    neighbors.add(pos)
-                }
-            }
-        }
-
-        val sequences = mutableSetOf<DigitSequence>()
-
-        neighbors.forEach { pos ->
-            if (pos.value?.isDigit() == true) {
-                val digitSeq = pos.getDigitSequence()
-                if (digitSeq != null) {
-                    sequences.add(digitSeq)
-                }
-            }
-        }
-
-        if (sequences.size != 2)
-            return null
-
-        return sequences.map { it.toInt() }.reduce { acc: Int, current: Int -> acc * current }
     }
 
     override fun part2() {

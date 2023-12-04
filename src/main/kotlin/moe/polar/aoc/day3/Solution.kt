73b32d7e73e0606ef846ee2cf99b20e8ad454dc4
httpolar <http@polar.moe>
2023-12-05 00:24:01 +0400

    Day 3, Part 2

--- src/main/kotlin/moe/polar/aoc/day3/Solution.kt
+++ src/main/kotlin/moe/polar/aoc/day3/Solution.kt
@@ -7,13 +7,23 @@ class Solution : DaySolution() {
     private val lines = text.lines()
     private val arr = Array(lines.size) { row -> CharArray(lines.first().length) { col -> lines[row][col] } }
 
+    private fun Array<CharArray>.hasValueAt(rowIdx: Int, colIdx: Int): Boolean {
+        return rowIdx >= 0 && rowIdx <= this.lastIndex && colIdx >= 0 && colIdx <= this.first().lastIndex
+    }
+
     private data class MatrixPos(var rowIdx: Int, var colIdx: Int)
 
     private fun MatrixPos.exists(): Boolean {
-        return rowIdx >= 0 && rowIdx <= arr.lastIndex && colIdx >= 0 && colIdx <= arr.first().lastIndex
+        return arr.hasValueAt(rowIdx, colIdx)
     }
 
+    private val MatrixPos.value: Char?
+        get() = arr.getOrNull(rowIdx)?.getOrNull(colIdx)
+
     private data class DigitSequence(var rowIdx: Int, var startIdx: Int, var endIdx: Int)
+
+    private val DigitSequence.indices: IntRange
+        get() = startIdx..<endIdx + 1
 
     private fun DigitSequence.toInt(): Int {
         var num = arr[rowIdx][startIdx].digitToInt()
@@ -103,12 +113,125 @@ class Solution : DaySolution() {
         println(sum)
     }
 
+
+    private fun MatrixPos.getDigitSequence(): DigitSequence? {
+        var left = colIdx
+        var foundLeft = false
+
+        if (!arr[rowIdx][left].isDigit())
+            return null
+
+        var right = colIdx + 1
+        var foundRight = false
+
+        while (!foundLeft || !foundRight) {
+            val chLeft = arr[rowIdx].getOrNull(left)
+            val chRight = arr[rowIdx].getOrNull(right)
+
+            val chNextLeft = arr[rowIdx].getOrNull(left - 1)
+            val chNextRight = arr[rowIdx].getOrNull(right + 1)
+
+            // make sure current characters are digits
+            if (chLeft?.isDigit() != true) {
+                foundLeft = true
+                left++
+            }
+
+            if (chRight?.isDigit() != true) {
+                foundRight = true
+                right--
+            }
+
+            // check if next characters are digits
+            if (!foundLeft && chNextLeft?.isDigit() != true) {
+                foundLeft = true
+            }
+
+            if (!foundRight && chNextRight?.isDigit() != true) {
+                foundRight = true
+            }
+
+            // move pointers
+            if (!foundLeft) {
+                left--
+            }
+
+            if (!foundRight) {
+                right++
+            }
+        }
+
+        return DigitSequence(rowIdx, left, right)
+    }
+
+    private fun MatrixPos.gearValue(): Int? {
+        if (!exists())
+            return null
+
+        val ch = arr[rowIdx][colIdx]
+        if (ch != '*')
+            return null
+
+        val neighbors = ArrayDeque<MatrixPos>()
+
+        for (i in rowIdx - 1..rowIdx + 1) {
+            for (j in colIdx - 1..colIdx + 1) {
+                if (i == rowIdx && j == colIdx)
+                    continue
+
+                val pos = MatrixPos(i, j)
+                if (pos.exists()) {
+                    neighbors.add(pos)
+                }
+            }
+        }
+
+        val sequences = mutableSetOf<DigitSequence>()
+
+        neighbors.forEach { pos ->
+            if (pos.value?.isDigit() == true) {
+                val digitSeq = pos.getDigitSequence()
+                if (digitSeq != null) {
+                    sequences.add(digitSeq)
+                }
+            }
+        }
+
+        if (sequences.size != 2)
+            return null
+
+        return sequences.map { it.toInt() }.reduce { acc: Int, current: Int -> acc * current }
+    }
+
     override fun part2() {
-        TODO("Not yet implemented")
+        val rows = arr.size
+        val cols = arr.first().size
+
+        var rowIdx = 0
+        var colIdx = 0
+
+        var sum = 0
+
+        while (rowIdx < rows) {
+            while (colIdx < cols) {
+                val ch = arr[rowIdx][colIdx]
+                if (ch == '*') {
+                    MatrixPos(rowIdx, colIdx).gearValue()?.let { sum += it }
+                }
+
+                colIdx++
+            }
+
+            colIdx = 0
+            rowIdx++
+        }
+
+        println(sum)
     }
 }
 
 fun main() {
     val s = Solution()
     s.part1()
+    s.part2()
 }

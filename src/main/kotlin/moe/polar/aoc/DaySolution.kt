4a39ea25da7da83d005b50e9ef712632e4063f87
httpolar <http@polar.moe>
2023-12-08 14:16:56 +0400

    Updated solution abstract class with one more method

--- src/main/kotlin/moe/polar/aoc/DaySolution.kt
+++ src/main/kotlin/moe/polar/aoc/DaySolution.kt
@@ -1,9 +1,15 @@
 package moe.polar.aoc
 
+import java.net.URL
+
 abstract class DaySolution {
     fun input(day: Int): String {
         return this::class.java.getResource("/day-$day.txt")?.readText()
             ?: error("Missing input file for day $day")
+    }
+
+    fun resource(name: String): URL? {
+        return this::class.java.getResource(name)
     }
 
     abstract fun part1()
